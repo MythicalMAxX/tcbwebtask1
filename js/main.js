802cfd696dd91c2749f1fd5a6a492727cda4e399
Vinamra Yadav <73683711+MythicalMAxX@users.noreply.github.com>
2022-08-17 11:43:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 
+const col = () => {
+   let color = '#';
+   for (let i = 0; i < 6; i++){
+      const random = Math.random();
+      const bit = (random * 16) | 0;
+      color += (bit).toString(16);
+   };
+   return color;
+};
+
+function init() { 
+        document.getElementById("test1").style.color = col();
+    }
